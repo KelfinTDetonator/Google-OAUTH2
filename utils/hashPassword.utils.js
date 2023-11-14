b386bfed3537aa3ecada05a99d37b71728a73b11
KelfinTDetonator <kelfinantoni@gmail.com>
2023-11-15 01:41:04 +0700

    fix: hash password util

--- utils/hashPassword.utils.js
+++ utils/hashPassword.utils.js
@@ -3,11 +3,24 @@ const bcrypt = require('bcrypt')
 module.exports = {
     hashPass: async function(password){
         try {
+            if(typeof password !== 'string'){
+                throw new Error("Wrong password data type")
+            }
             const salt = await bcrypt.genSalt();
             return bcrypt.hash(password, salt)
         } catch (error) {
-            console.error(error)
-            throw Error(error)
+            throw error
+        }
+    },
+    verifyPassword: async function(password, encryptedPass){
+        try {
+            if(typeof password !== 'string'){
+                throw new Error("Wrong password data type")
+            }
+
+            return bcrypt.compare(password, encryptedPass)
+        } catch (error) {
+            throw error
         }
     }
 }
